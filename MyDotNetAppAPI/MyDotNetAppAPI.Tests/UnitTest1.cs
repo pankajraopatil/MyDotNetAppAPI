bf13b82a2bf9e31a742f25db66155702940f071d
Pankaj Patil <PankajraoPatil@gmail.com>
2026-05-09 15:03:13 +0530

    Initial commit: .NET Core Web API with tests

---
+++ MyDotNetAppAPI/MyDotNetAppAPI.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace MyDotNetAppAPI.Tests;
+
+public class SimpleTest
+{
+    [Fact]
+    public void Test_AlwaysPasses()
+    {
+        // Arrange
+        bool result = true;
+
+        // Assert
+        Assert.True(result, "This test should always pass!");
+    }
+
+    [Fact]
+    public void Test_StringIsNotNull()
+    {
+        // Arrange
+        string message = "Hello CI/CD!";
+
+        // Assert
+        Assert.NotNull(message);
+    }
+}
